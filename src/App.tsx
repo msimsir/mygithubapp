d38f154913f180ff6c4e2b7aebb4a04e18dba727
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2021-04-15 11:54:23 +0300

    added UserDetails and Bookmarks route and updated global styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Bookmark from "./pages/Bookmark/Bookmark";
 import Home from "./pages/Home/Home";
 import RepoDetails from "./pages/RepoDetails/RepoDetails";
 import SearchResults from "./pages/SearchResults/SearchResults";
+import UserDetails from "./pages/UserDetails/UserDetails";
 
 const App: React.FC = () => {
   return (
@@ -12,6 +14,8 @@ const App: React.FC = () => {
           <Route path="/" exact component={Home} />
           <Route path="/search" component={SearchResults} />
           <Route path="/repodetail" component={RepoDetails} />
+          <Route path="/userdetail" component={UserDetails} />
+          <Route path="/bookmarks" component={Bookmark} />
         </Switch>
       </Router>
     </>
